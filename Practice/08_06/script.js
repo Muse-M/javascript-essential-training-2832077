9b8812a3a6c4e5cb5ab8caa43e6a4c60045e3d40
Muse-M <97995635+Muse-M@users.noreply.github.com>
2024-08-19 15:08:54 +0000

    Ch 6 and Ch 8 Practice

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,29 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+const function1 = function simpleDeclerationFunction(param) {
+    const main = document.querySelector("main")
+    main.innerHTML =param
+};
+
+let newHTML= `
+  <article>
+    <h1 class="backpack__name">"Everyday backpack"</h1>
+    </article>`
+function1(newHTML);
+
+var param2 = `
+  <article>
+    <h1 class="backpack__name">"New title"</h1>
+    </article>`
+
+const function2 = param2 => {
+    const main = document.querySelector("main")
+    main.innerHTML =param2
+}
+
+function2(param2)
+
+
+
